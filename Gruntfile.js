ece210edbc76106fb261956b3b884160121dd10c
last1here <last1here@hotmail.co.uk>
2014-08-15 22:20:57 +0100

    Remove Gruntfile.js and package.json from .gitignore

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded',
+          banner: '/*!\n' +
+            ' * scss_grid v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+            ' * Copyright 2014-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
+            ' */\n',
+        },
+        files: {
+          'css/<%= pkg.name %>.css': 'scss/<%= pkg.name %>.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: ['scss/**/*.scss', 'scss/**/_*.scss'],
+        tasks: ['sass']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default',['watch']);
+}
